b6b3326568df249f1715f73b8b38c35cee24e736
Robert Rothenberg <rrwo@cpan.org>
2025-07-11 22:18:15 +0100

    Add initial prototype

---
+++ lib/Software/Policy/CodeOfConduct.pm
@@ -0,0 +1,52 @@
+package Software::Policy::CodeOfConduct;
+
+use v5.20;
+
+use Moo;
+
+use File::ShareDir qw( module_file );
+use Text::Template;
+
+use experimental qw( signatures );
+
+has contact => (
+    is       => 'ro',
+    required => 1,
+);
+
+has policy => (
+    is      => 'ro',
+    default => 'Contributor_Covenant',
+);
+
+has template_path => (
+    is      => 'lazy',
+    builder => sub($self) {
+        return module_file( __PACKAGE__, $self->policy . ".md.tmpl", );
+    },
+);
+
+has template => (
+    is      => 'lazy',
+    builder => sub($self) {
+        return Text::Template->new(
+            TYPE   => "FILE",
+            SOURCE => $self->template_path,
+        );
+    }
+
+);
+
+has text => (
+    is      => 'lazy',
+    builder => sub($self) {
+        return $self->template->fill_in(
+            HASH => {
+                contact => $self->contact,
+            }
+        );
+
+    }
+);
+
+1;
---
+++ t/01-basic.t
@@ -0,0 +1,21 @@
+
+use v5.20;
+use warnings;
+
+use Test2::V0;
+
+use Test::File::ShareDir -share => {
+    -module => {
+        "Software::Policy::CodeOfConduct" => "share"
+    }
+};
+
+use Software::Policy::CodeOfConduct;
+
+ok my $policy = Software::Policy::CodeOfConduct->new( contact => 'bogon@example.com' ), 'constructor';
+
+ok $policy->template_path, "template_path";
+
+ok $policy->text, "text";
+
+done_testing;
